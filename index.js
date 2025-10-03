e497c99b03af25f2dec98dc38139f7da12e6fe37
Kai <100989619+kaienkhoo@users.noreply.github.com>
2025-10-03 15:51:37 +0800

    Basic calculator operation is done

--- index.js
+++ index.js
@@ -1,11 +1,66 @@
-const buttons = document.querySelectorAll('.btn')
+const buttons = document.querySelectorAll('button')
 const display = document.querySelector('.display-text')
+
+
+let first = ''
+let second = ''
+let op = ''
+let isTypingSecond = false
+
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
-        display.innerText = e.target.innerText
+        const val = e.target.innerText
+
+        if(/^\d$/.test(val)){
+            if(!op){
+                first += val
+                display.textContent = first
+            } else {
+                second += val
+                display.textContent = second
+            }
+        }
+
+
+        if("+-*/".includes(val)) {
+            if(!first){
+                return
+            }
+
+            if(!second){
+                op = val
+            }
+
+            isTypingSecond = true
+        }
+        
+        if(val === "="){
+            if(first && op && second){
+                const result = operate(op,Number(first),Number(second))
+                display.textContent = result
+                first = String(result);
+                second = "";
+                op = "";
+                isTypingSecond = false;
+            }
+
+            return
+        }
+
+        if (val.toLowerCase() === "c") {
+            first = second = op = "";
+            isTypingSecond = false;
+            display.textContent = "0";
+            return;
+        }
+
+        console.log(first,second,op)
+
     })
 })
+
+
 
 
 function add(x,y) {
